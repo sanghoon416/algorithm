ab1244e97b54a49bca4ffee070b61505084d6972
“sanghoon416” <jk08097@naver.com>
2024-09-25 15:02:56 +0900

    9월 코테 3번 최종

--- codingTest_9/tempCodeRunnerFile.cpp
+++ codingTest_9/tempCodeRunnerFile.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int n;
 long long num[3005];
-long long ans = 0;
+int ans = 0;
 
 void input(){
 	cin >> n;
@@ -21,16 +21,18 @@ void input(){
 void solve(){
     for(int i=0;i<n-2;i++){
         for(int j=i+1;j<n-1;j++){
-            long long x = num[i];
-            long long y = num[j];
+            int x = num[i];
+            int y = num[j];
 
             int cnt = lower_bound(num+j+1,num+n,x+y) - num;
+            cout << "x:" << x << " y:" << y << " z:" << num[cnt] << " cnt:" << cnt << " j:" << j << "\n";
             if(num[cnt] == x+y){
                 cnt = cnt - j;
             }
             else {
                 cnt = cnt - j - 1;
             }
+
             ans += cnt;
         }
     }
